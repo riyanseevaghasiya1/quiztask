cce331e39d27407d0373c2723a024d90543e4825
Dhruvi <dhruvi.kalathiyainfotech12@gmail.com>
2026-01-13 17:53:30 +0530

    riyansee13-01

--- quiz.js
+++ quiz.js
@@ -1,1 +1,8 @@
-console.log("hello")
+let data = [
+    {id:1,question:"What is the basic unit of life?",option:"A:Organelle;B:Cell,C:Tissue,D:Molecule"},
+    {id:2,question:"What is the chemical symbol for water",option:"A:O2;B:H2O;C:CO2;D:NaCl"},
+    {id:3,question:"Which planet is known as the Red Planet?",option:"A:Venus;B:Jupiter;C:Mars,D:Saturn"},
+    {id:4,question:"Which is the largest ocean in the world?",option:"A:Atlantic Ocean;B:Indian Ocean;C:Arctic Ocean,D:Pacific Ocean"},
+    {id:4,question:"Which country is known as the Land of the Rising Sun?",option:"A:China;B:Japan;C:South Korea,D:Thailand"},
+
+]
